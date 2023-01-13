9b83cefcdc40aab3f3990da4201ae590b39043b7
Gabriel C <gabriel.camargo96@hotmail.com.br>
2023-01-13 13:02:18 -0300

    feat: prints repeated elements in 2 given string arrays

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String[] frutas1 = { "Morango", "Uva", "Acerola", "Manga" };
+        String[] frutas2 = { "Pêra", "Caju", "Morango", "Kiwi" };
+
+        List<String> repeated = new ArrayList<String>();
+
+        for (int i = 0; i < frutas1.length; i++) {
+            for (int j = 0; j < frutas2.length; j++) {
+                if (frutas1[i].equalsIgnoreCase(frutas2[j]) && !repeated.contains(frutas1[i])) {
+                    repeated.add(frutas1[i]);
+                    System.out.println(frutas1[i]);
+                }
+            }
+        }
     }
 }
